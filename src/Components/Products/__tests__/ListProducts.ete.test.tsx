8ccc86cdbb337b123ea3d60e7c0267e2879f026f
ijoakin2017 <ijoakin@gmail.com>
2021-03-20 20:36:10 -0300

    added pupperteer

---
+++ src/Components/Products/__tests__/ListProducts.ete.test.tsx
@@ -0,0 +1,9 @@
+import puppeteer from 'puppeteer';
+
+describe("end to end testing list Products", () => {
+  it("testing", async ()  => {
+    const browser = await puppeteer.launch();
+
+    
+  })
+});
